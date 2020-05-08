292b7d5c2584410bd64551b7bd42d481a4763bb5
白纪良 <bjl_mx@163.com>
2020-05-08 11:47:24 +0800

    2020.5.8

---
+++ src/main/java/com/data/datawaytest/entity/ImageVO.java
@@ -0,0 +1,13 @@
+package com.data.datawaytest.entity;
+
+import lombok.Data;
+
+/** @author Baijl
+ * @date 2020-04-26
+ * @time 15:44
+ * @description
+ */
+@Data
+public class ImageVO {
+    private String url;
+}
---
+++ src/main/java/com/data/datawaytest/exceptionhandler/FindException.java
@@ -0,0 +1,15 @@
+package com.data.datawaytest.exceptionhandler;
+
+/** @author Baijl
+ * @date 2020-05-04
+ * @time 15:27
+ * @description
+ */
+public class FindException extends Exception {
+
+    private static final long serialVersionUID = -4514818915484263414L;
+
+    public FindException(String message) {
+        super(message);
+    }
+}
